142384016e53c8fd88d812dbe5dcee77b0bea84c
Gourab Das <dgourab574@gmail.com>
2026-04-02 00:54:44 +0530

    Timeline updated

--- src/constants/Timeline_Data.js
+++ src/constants/Timeline_Data.js
@@ -1,37 +1,44 @@
 export const timelineData = [
     {
-      year: "2015",
-      title: "Humble Beginnings",
-      description: "Avenir started a small departmental initiative with 200+ participants. Driven by passion and curiosity, it laid the foundation of a culture based on innovation and collaboration.",
+      year: "2003",
+      title: "Origins",
+      description: "Avenir started off as a small departmental initiative with 100+ participants in 2003. It was set up with the help from Kshitij of Indian Institute of Technology, Kharagpur.",
       side: "left",
       // icon: "🌱" 
     },
     {
-      year: "2018",
-      title: "Going College-Wide",
-      description: "The event expanded to include all departments crossing 1000+ participants. It transformed from a small initiative to a unified celebration of technology and innovation.",
+      year: "2006",
+      title: "Humble beginnings",
+      description: "Under Phoenix, the event expanded to include all departments crossing 1000+ participants. The motto was to look beyond academics, and inculcate in the students the zeal of co-curricular activities. Driven by passion and curiosity, it laid the foundation of a culture based on innovation and collaboration. ",
       side: "right",
       // icon: "🏫"
+    },
+    {
+      year: "2014",
+      title: "Beyond college",
+      description: "Avenir had its major breakthrough in 2014 recording a massive footfall of over 4,000 participants. Avenir was thus established as a celebrated tech fest of the state.",
+      side: "left",
+      // icon: "💻"
     },
     {
       year: "2020",
       title: "Virtual Innovation",
       description: "Amid the pandemic, Avenir successfully transitioned into a fully virtual event. This shift proved its adaptability while maintaining the same standard of learning, competition and engagement.",
-      side: "left",
-      // icon: "💻"
-    },
-    {
-      year: "2023",
-      title: "National Recognition",
-      description: "Avenir grew into one of Kolkata's premier technical festivals. With wider participation and bolder partnerships, it earned national level recognition.",
       side: "right",
       // icon: "🏆"
     },
     {
-      year: "2026",
-      title: "A promise for the future ",
-      description: "Avenir 26 brings a promise of excitement, high octane events, that compels willing participants to rack their brains and navigate through the streets of Avenir City. With the official theme as GTA - GRAND THEFT AUTO, the world  famous gaming franchise. With students from over 50+ different institutions spanning across Kolkata and beyond, this is the moment to win it all.",
+      year: "2023",
+      title: "National Recognition",
+      description: "Avenir has grown into one of Kolkata's premier techno-managerial festivals. With wider participation and bolder partnerships, it has earned national level recognition.",
       side: "left",
+      // icon: "✨"
+    },
+    {
+      year: "2026",
+      title: "A promise for the future",
+      description: "Avenir 26 brings a promise of excitement and high octane events that compels willing participants to rack their brains and navigate through the streets of Avenir City. The official theme this time round is GTA - GRAND THEFT AUTO, the globally recognised gaming franchise. With expected participation of students from over 50+ different institutions spanning across Kolkata and beyond, the doors of Avenir city are open to all brave challengers aspiring to win big!",
+      side: "right",
       // icon: "✨"
     },
   ];
